4bccbe913866bf82c293e3acaf39a5e4f0cf2043
Chaos <tobias.floor@gmail.com>
2023-03-02 22:36:57 +0100

    Added version 6 video

--- ProcessFrames.py
+++ ProcessFrames.py
@@ -1,6 +1,6 @@
 import os, sys, getopt, cv2, shutil, PickleUtil
 
-def Frames2Features(inputfolder, outputfolder, filenameformat):
+def Frames2Features(inputfolder, outputfolder, max_keypoints, filenameformat):
     # Remove older version of the processed video if it exists
     if (os.path.exists(outputfolder)):
         shutil.rmtree(outputfolder)
@@ -25,9 +25,11 @@ def Frames2Features(inputfolder, outputfolder, filenameformat):
         
         tmpkeys, desc = sift.detectAndCompute(gray, None)
         
-        image=cv2.drawKeypoints(gray,tmpkeys,img,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        largest_keypoints = sorted(tmpkeys, key=lambda point: point.size, reverse=True)[:max_keypoints]
         
-        keyp = [(point.pt, point.size, point.angle, point.response, point.octave, point.class_id, desc) for point in tmpkeys]
+        image=cv2.drawKeypoints(gray,largest_keypoints,img,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+        
+        keyp = [(point.pt, point.size, point.angle, point.response, point.octave, point.class_id, desc) for point in largest_keypoints]
         
         # Save the frame as a JPEG image file
         filename = os.path.join(outputfolder, file)
@@ -40,15 +42,19 @@ def Frames2Features(inputfolder, outputfolder, filenameformat):
 if __name__ == "__main__":
     inputfolder = ''
     outputfolder = ''
-    opts, args = getopt.getopt(sys.argv[1:], "hi:o:", ["input=", "output="])
+    max_keypoints = 100
+    opts, args = getopt.getopt(sys.argv[1:], "hi:o:m:", ["input=", "output=", "max_keypoints="])
     for opt, arg in opts:
         if opt == '-h':
-            print ('python ProcessFrames.py -i <inputfolder> -o <outputfolder>')
+            print ('python ProcessFrames.py -i <inputfolder> -o <outputfolder> -m <max number of keypoints>')
             sys.exit()
         elif opt in ("-i"):
             inputfolder = arg
         elif opt in ("-o"):
             outputfolder = arg
+        elif opt in ("-m"):
+            max_keypoints = int(arg)
     print('Input folder is', inputfolder)
     print('Output folder is', outputfolder)
-    Frames2Features(inputfolder, outputfolder, "frame{0}.jpg")
+    print('Max number of keypoints is', max_keypoints)
+    Frames2Features(inputfolder, outputfolder, max_keypoints, "frame{0}.jpg")
